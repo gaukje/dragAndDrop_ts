e51c9649904c468128c8ca21e85caa0c0d28fde2
Gaute Zhang Kjelstadli <gzkjelstadli@gmail.com>
2024-02-07 11:06:28 +0100

    Bruker ES Modules

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,7 @@
-/// <reference path="models/drag-drop.ts" />
-/// <reference path="models/project.ts" />
-/// <reference path="state/project-state.ts" />
-/// <reference path="util/validation.ts" />
-/// <reference path="decorators/autobind.ts" />
-/// <reference path="component/base-component" />
-/// <reference path="component/project-input" />
-/// <reference path="component/project-item" />
-/// <reference path="component/project-list" />
+import { ProjectInput } from './component/project-input.js';
+import { ProjectList } from './component/project-list.js';
 
 
-namespace App {
-
-    new ProjectInput();
-    new ProjectList('active');
-    new ProjectList('finished');
-
-}
+new ProjectInput();
+new ProjectList('active');
+new ProjectList('finished');
